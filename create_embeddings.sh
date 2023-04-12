61156d13a0c2db93fc4c590fe947bd1e901ba37f
jimenaRL <jimena@melix.net>
2023-04-12 16:51:18 +0200

    Minor changes

--- create_embeddings.sh
+++ create_embeddings.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-declare -a Limits=(1e4 1e5 1e6 1e7 1.1e7)
+# declare -a Limits=(1e4 1e5 1e6 1e7 1.1e7)
+
+declare -a Limits=(1e5)
 
 for limit in ${Limits[@]}; do
-    python python/dev.py --config=france.yaml --limit=$limit --output=images
+    python python/dev.py --config=france.yaml --limit=$limit --output=outputs
 done
