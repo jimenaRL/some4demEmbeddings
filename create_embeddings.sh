16835f9d8e4cce7f37369738ae06f1dd65ba1bb0
jimenaRL <jimena@melix.net>
2023-04-27 11:20:27 +0200

    Add germany conf

--- create_embeddings.sh
+++ create_embeddings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-python python/create_ide_emb.py --config=france.yaml --output=outputs
-python python/create_att_emb.py --config=france.yaml --output=outputs
-python python/viz_emb.py --config=france.yaml --output=outputs
+python python/create_ide_emb.py --config=germany.yaml --output=outputs
+# python python/create_att_emb.py --config=germany.yaml --output=outputs
+# python python/viz_emb.py --config=germany.yaml --output=outputs
