5f421e1ea87309b5d3497aaa064e1cc2d3041cd7
jimenaRL <jimena@melix.net>
2023-05-15 12:43:19 +0200

    Fectorize pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-declare -a StringArray=(france)
+declare -a StringArray=(
+    # france
+    # germany
+    # spain
+    # italy
+    # belgium
+    # netherlands
+    # # romania  # create_ide_emb fails on SVD computation with prince
+    # slovenia
+    # poland
+)
 
 outputs=outputs
 suffix=15D.yaml
 
 for country in ${StringArray[@]}; do
-    python python/some4demexp/embeddings/preprocess_data.py --config=config$suffix --country=$country --output=$outputs
-    python python/some4demexp/embeddings/create_ide_emb.py --config=config$suffix --country=$country --output=$outputs
-    python python/some4demexp/embeddings/create_att_emb.py --config=config$suffix  --country=$country --output=$outputs
+
+    echo "################# ${country} #################"
+
+    # python python/some4demexp/embeddings/preprocess_data.py --config=config$suffix --country=$country --output=$outputs
+    # python python/some4demexp/embeddings/create_ide_emb.py --config=config$suffix --country=$country --output=$outputs
+    # python python/some4demexp/embeddings/create_att_emb.py --config=config$suffix  --country=$country --output=$outputs
     python python/some4demexp/visualizations/matplotlib/create_2Dviz.py \
         --config=config$suffix \
         --country=$country \
         --vizconfig="vizconfigs/$country$suffix" \
         --output=$outputs \
-        --show
+        # --show
 done
