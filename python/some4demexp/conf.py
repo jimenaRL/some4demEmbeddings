fc3a90d85fcbf946a43e09b923dcd77e80de2ade
jimenaRL <jimena@melix.net>
2023-05-26 10:35:57 +0200

    configs and quantiles

---
+++ python/some4demexp/conf.py
@@ -0,0 +1,30 @@
+CHESLIMS = {
+  'lrgen': [0, 10],
+  'lrecon': [0, 10],
+  'antielite_salience': [0, 10],
+  'eu_position': [0, 7],
+  'immigrate_policy': [0, 10],
+  'galtan': [0, 10],
+  'environment': [0, 10],
+}
+
+ATTDICT = {
+    'lrgen': 'CHES Left – Right',
+    'eu_position': 'CHES EU Integration',
+    'antielite_salience': 'CHES Anti-elite Salience',
+    'lrecon': 'CHES Economic Left – Right',
+    'immigrate_policy': 'CHES Immigration Policy',
+    'galtan': 'CHES GAL – TAN',
+    'environment': 'CHES Environment – Economy'
+}
+
+ATTDIMISSUES = {
+    'lrgen': {
+        'issues': ['Left', 'Right'],
+        'only_pos': True
+    },
+    'antielite_salience': {
+        'issues': ['Elites', 'People', 'Politicians'],
+        'only_pos': False
+    }
+}
