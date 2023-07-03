695490a9656e1bb16e9039c173091f1b457dec0c
jimenaRL <jimena@melix.net>
2023-07-03 15:39:57 +0200

    REmove saving parties

--- python/some4demexp/stats.py
+++ python/some4demexp/stats.py
@@ -30,8 +30,6 @@ with open(config, "r", encoding='utf-8') as fh:
     params = yaml.load(fh, Loader=yaml.SafeLoader)
 print(yaml.dump(params, default_flow_style=False))
 
-ATTDIMS = params['attitudinal_dimensions']
-
 ide_folder = set_output_folder_emb(params, country, output)
 ide_sources, ide_targets = load_ide_embeddings(ide_folder)
 
@@ -45,8 +43,13 @@ ide_targets_stats.to_csv(os.path.join(ide_folder, "ide_targets_stats.csv"))
 
 print(f"Ideological embeddings stats saved at: {ide_folder}.")
 
+if not 'attitudinal_dimensions' in params:
+    exit()
+
+ATTDIMS = params['attitudinal_dimensions']
+
 att_folder = set_output_folder_att(params, country, output)
-att_sources, att_targets, att_groups = load_att_embeddings(att_folder)
+att_sources, att_targets = load_att_embeddings(att_folder)
 
 att_sources_stats = att_sources.describe()
 print(f"Attitudinal embeddings sources stats:\n{att_sources_stats}")
@@ -55,10 +58,5 @@ att_sources_stats.to_csv(os.path.join(att_folder, "att_sources_stats.csv"))
 att_targets_stats = att_targets.describe()
 print(f"Attitudinal embeddings targets stats:\n{att_targets_stats}")
 att_targets_stats.to_csv(os.path.join(att_folder, "att_targets_stats.csv"))
-
-att_groups_stats = att_groups.describe()
-print(f"Attitudinal embeddings parties stats:\n{att_groups_stats}")
-att_groups_stats.to_csv(os.path.join(att_folder, "att_groups_stats.csv"))
-
 
 print(f"Attitudinal embeddings stats saved at: {att_folder}.")
