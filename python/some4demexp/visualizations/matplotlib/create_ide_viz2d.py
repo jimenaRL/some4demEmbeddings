1c50841b7279cd31a272b82f48e778fab61e8d96
jimenaRL <jimena@melix.net>
2023-07-03 15:36:32 +0200

    Add parties to show

--- python/some4demexp/visualizations/matplotlib/create_ide_viz2d.py
+++ python/some4demexp/visualizations/matplotlib/create_ide_viz2d.py
@@ -41,7 +41,18 @@ ide_sources, ide_targets = load_ide_embeddings(ide_folder)
 data_folder = set_output_folder(params, country, output)
 targets_groups = load_targets_groups(data_folder)
 
-n_dims_to_viz = min(params['ideological_model']['n_latent_dimensions'], 4)
+#  ################### HOT FIX 1 ###################
+targets_groups = targets_groups.assign(
+    party=targets_groups.party.apply(lambda p: p.replace('&', ''))
+)
+#####################################################
+
+
+n_dims_to_viz = min(params['ideological_model']['n_latent_dimensions'], 8)
+
+palette = vizparams['palette']
+idevizparams = vizparams['ideological']
+parties_to_show = palette.keys()
 
 # visualize ideological space
 for x, y in combinations(range(n_dims_to_viz), 2):
@@ -51,8 +62,9 @@ for x, y in combinations(range(n_dims_to_viz), 2):
         targets_parties=targets_groups,
         latent_dim_x=x,
         latent_dim_y=y,
+        parties_to_show=parties_to_show,
         output_folder=ide_folder,
         show=show,
-        palette=vizparams['palette'],
-        **vizparams['ideological']
+        palette=palette,
+        **idevizparams
     )
