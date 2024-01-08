a7c4d06e62b6cf96e51cb8e0f153de6f49e1b0ee
jimenaRL <jimena@melix.net>
2024-01-08 15:38:59 +0100

    Update correlation matrices code

--- python/some4demexp/validation/correlation_matrices.py
+++ python/some4demexp/validation/correlation_matrices.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 
 from some4demdb import SQLite
 from some4demexp.inout import \
+    get_ide_ndims, \
     set_output_folder, \
     set_output_folder_att, \
     load_att_embeddings, \
@@ -19,8 +20,10 @@ from some4demexp.inout import \
     load_ide_embeddings
 
 from some4demexp.conf import \
-    CHESLIMS, \
-    ATTDICT
+    CHES2019LIMS, \
+    CHES2019ATTDICT, \
+    GPS2019LIMS, \
+    GPS2019ATTDICT
 
 from some4demexp.correlation_coefficient import smallSamplesSpearmanr
 
@@ -28,39 +31,62 @@ from some4demexp.correlation_coefficient import smallSamplesSpearmanr
 ap = ArgumentParser()
 ap.add_argument('--config', type=str, required=True)
 ap.add_argument('--country', type=str, required=True)
+ap.add_argument('--survey', type=str, required=True)
 ap.add_argument('--output', type=str, required=True)
 ap.add_argument('--show',  action='store_true', required=False)
 
 args = ap.parse_args()
 config = args.config
 country = args.country
+survey = args.survey
 output = args.output
 show = args.show
 
 with open(config, "r", encoding='utf-8') as fh:
     params = yaml.load(fh, Loader=yaml.SafeLoader)
-print(yaml.dump(params, default_flow_style=False))
 
-ATTDIMS = params['attitudinal_dimensions']
-NIDEDIMS = params['ideological_model']['n_latent_dimensions']
-IDEDIMS = range(NIDEDIMS)
+with open(params['params_db'], "r", encoding='utf-8') as fh:
+    params_db = yaml.load(fh, Loader=yaml.SafeLoader)
+
+SQLITE = SQLite(
+    params['sqlite_db'].format(country=country),
+    params_db['output']['tables'],
+    country)
+
+ATTDIMS = params['attitudinal_dimensions'][survey]
+parties_mapping = SQLITE.getPartiesMapping()
+ideN = get_ide_ndims(parties_mapping, survey)
+IDEDIMS = range(ideN)
 
 IDEDIMSNAMES = [fr'$\delta_{j+1}$' for j in IDEDIMS]
+ATTDICT = locals()[f"{survey.upper()}ATTDICT"]
 ATTDIMSNAMES = [ATTDICT[i] for i in ATTDIMS]
+SURVEYCOL = f'{survey.upper()}_party_acronym'
 
-SQLITE = SQLite(params['sqlite_db'])
+cmap_s = sns.diverging_palette(145, 300, s=60, as_cmap=True)
+
+SMALL_SAMPLES_METHODS = [
+    ("Pearson", stats.pearsonr, "vlag"),
+    ("Spearman", smallSamplesSpearmanr, cmap_s),
+]
+
+LARGE_SAMPLES_METHODS = [
+    ("Pearson",  stats.pearsonr, "vlag"),
+    ("Spearman", stats.spearmanr, cmap_s),
+]
 
 
 def compute_correlation_matrices(
     ide_data, att_data, info, methods, pvalue_display_th=0.05):
+
 
     for method, correlation_fn, cmap in methods:
 
         fig = plt.figure(figsize=(16, 8))
         ax = fig.add_subplot(1,  1,  1)
 
-        correlations = np.empty((len(ATTDIMS), NIDEDIMS), float)
-        annotations = np.empty((len(ATTDIMS), NIDEDIMS), object)
+        correlations = np.empty((len(ATTDIMS), ideN), float)
+        annotations = np.empty((len(ATTDIMS), ideN), object)
 
         for i, attdim in enumerate(ATTDIMS):
             for j in IDEDIMS:
@@ -103,7 +129,7 @@ def compute_correlation_matrices(
         ax.set(xlabel="", ylabel="")
         ax.xaxis.tick_top()
         _title = f"{method} Coeficient Correlation Matrix \n"
-        _title += f"{country.capitalize()} N={NIDEDIMS}  M={len(ATTDIMS)}\n"
+        _title += f"{country.capitalize()} N={ideN}  M={len(ATTDIMS)}\n"
         _title += info
         _title += f"\np-values less than {pvalue_display_th} displayed"
         plt.title(_title)
@@ -119,62 +145,54 @@ def compute_correlation_matrices(
         plt.show()
 
 
-# (0) Get embeddings
+# (0) Get embeddings and parties assignations
 folder = set_output_folder(params, country, output)
 
-ide_folder = set_output_folder_emb(params, country, output)
+ide_folder = set_output_folder_emb(
+    params, country, survey, ideN, output)
 ide_sources, ide_targets = load_ide_embeddings(ide_folder)
 
-att_folder = set_output_folder_att(params, country, output)
+att_folder = set_output_folder_att(params, survey, country, ideN, output)
 att_sources, att_targets = load_att_embeddings(att_folder)
 
-cmap_s = sns.diverging_palette(145, 300, s=60, as_cmap=True)
+
+targets_parties = SQLITE.getMpParties(['MMS', survey])
+
+ide_targets = ide_targets.merge(
+        targets_parties,
+        left_on="entity",
+        right_on="mp_pseudo_id",
+        how="inner"
+    ) \
+    .drop(columns="mp_pseudo_id")
+
+
 
 # (1) Compute correlation matrix IDE vs CHES for parties
 
-groups_coord_att = SQLITE.retrieveAndFormatTargetGroupsAttitudes(country, ATTDIMS)
-parties_mapping = SQLITE.getPartiesMapping(country)
-
-
-l0 = len(ide_targets)
-ide_targets = ide_targets.merge(att_targets[["party", "entity"]])
-if not l0 == len(ide_targets):
-    raise ValueError(f"Target loss during merge.")
-
-valid_parties = set(groups_coord_att.party) & set(ide_targets.party.to_list())
-ide_targets = ide_targets[ide_targets.party.isin(valid_parties)]
-groups_coord_att = groups_coord_att[groups_coord_att.party.isin(valid_parties)]
+parties_coord_att = SQLITE.getPartiesAttitudes(survey, ATTDIMS) \
+    .drop(columns='MMS_party_acronym')
 
 
 estimated_groups_coord_ide = ide_targets \
-    .groupby('party') \
+    .drop(columns=['entity', 'MMS_party_acronym']) \
+    .groupby(SURVEYCOL) \
     .mean() \
     .reset_index()
 
-small_samples_methods = [
-    ("Pearson", stats.pearsonr, "vlag"),
-    ("Spearman", smallSamplesSpearmanr, cmap_s),
-]
-
 compute_correlation_matrices(
-    estimated_groups_coord_ide.sort_values(by='party'),
-    groups_coord_att.sort_values(by='party'),
+    estimated_groups_coord_ide.sort_values(by=SURVEYCOL),
+    parties_coord_att.sort_values(by=SURVEYCOL),
     "PARTIES",
-    small_samples_methods)
+    SMALL_SAMPLES_METHODS)
 
 
-# (2) Compute correlation matrix IDE vs CHES for parties
+# # (2) Compute correlation matrix IDE vs CHES for parties
 
-data =  ide_sources.merge(att_sources,  on='entity',  how='inner')
-
-large_samples_methods = [
-    ("Pearson",  stats.pearsonr, "vlag"),
-    ("Spearman", stats.spearmanr, cmap_s),
-]
-
-compute_correlation_matrices(
-    data,
-    data,
-    "SOURCES",
-    large_samples_methods)
+# data =  ide_sources.merge(att_sources,  on='entity',  how='inner')
+# compute_correlation_matrices(
+#     data,
+#     data,
+#     "SOURCES",
+#     LARGE_SAMPLES_METHODS)
 
